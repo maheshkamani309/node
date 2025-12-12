fa144dfb2cbe5e962db4161dc6067959edf66cc3
Mahesh <maheshkamani309@gmail.com>
2025-12-12 11:40:03 +0530

    Express Routes

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
-const http = require('http');
-const {handler}   = require('./routes');
+const http      = require('http');
+const express   = require('express');
 
-const server = http.createServer(handler);
+const app  = express();
 
-server.listen(3000);
+    app.use((req, res, next) => {
+        console.log("First middleware");
+        next();
+    });
+
+
+    app.use((req, res, next)=>{
+        console.log("Second request");
+        res.send('<h1>This is the testing.</h1>')
+    });
+
+app.listen(3000)
