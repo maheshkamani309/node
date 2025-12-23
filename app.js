521c884853c41dfd770b02c3aa8162cb44c58b4d
Mahesh <maheshkamani309@gmail.com>
2025-12-23 16:35:39 +0530

    Working with admin and front concept

--- app.js
+++ app.js
@@ -12,13 +12,11 @@ app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
-const adminUsers = require('./routes/users');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin', adminRoutes);
-app.use('/admin/users', adminUsers);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
